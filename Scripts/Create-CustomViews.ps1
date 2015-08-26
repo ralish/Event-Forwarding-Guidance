0f8907034fd25c31b065173e0746d7f565b6a01f
Samuel D. Leslie <sdl@nexiom.net>
2015-08-27 01:42:41 +1000

    Restore support for adding time filters to generated Custom Views

--- Scripts/Create-CustomViews.ps1
+++ Scripts/Create-CustomViews.ps1
@@ -15,6 +15,9 @@
     .PARAMETER CustomViewsPath
     Specifies the directory where generated Custom Views will be saved.
 
+    .PARAMETER TimeFilter
+    A TimeSpan object provided by Get-TimeSpan used to filter custom views.
+
     .LINK
     https://www.nsa.gov/ia/_files/app/Spotting_the_Adversary_with_Windows_Event_Log_Monitoring.pdf
 #>
@@ -27,7 +30,10 @@ Param(
 
     [Parameter(Mandatory=$true)]
     [ValidateNotNullOrEmpty()]
-    [String]$CustomViewsPath
+    [String]$CustomViewsPath,
+
+    [Parameter(Mandatory=$false)]
+    [TimeSpan]$TimeFilter
 )
 
 # Ensure that any errors we receive are considered fatal
@@ -38,14 +44,28 @@ Set-Variable -Name CvFileStart -Option Constant -Scope Script -Value "<ViewerCon
 Set-Variable -Name CvFileEnd -Option Constant -Scope Script -Value "`n`t`t</QueryNode>`n`t</QueryConfig>`n</ViewerConfig>"
 Set-Variable -Name CvNameStart -Option Constant -Scope Script -Value "`n`t`t`t<Name>"
 Set-Variable -Name CvNameEnd -Option Constant -Scope Script -Value "</Name>"
-Set-Variable -Name CvQueryStart -Option Constant -Scope Script -Value "`n`t`t`t<QueryList>`n`t`t`t`t<Query Id=`"0`">`n"
-Set-Variable -Name CvQueryEnd -Option Constant -Scope Script -Value "`t`t`t`t</Query>`n`t`t`t</QueryList>"
+Set-Variable -Name CvQueryStart -Option Constant -Scope Script -Value "`n`t`t`t<QueryList>`n`t`t`t`t<Query Id=`"0`">"
+Set-Variable -Name CvQueryEnd -Option Constant -Scope Script -Value "`n`t`t`t`t</Query>`n`t`t`t</QueryList>"
+Set-Variable -Name CvFwdEvtStart -Option Constant -Scope Script -Value "`n    <Select Path=`"ForwardedEvents`">"
+Set-Variable -Name CvFwdEvtEnd -Option Constant -Scope Script -Value "`n    </Select>"
 
 Function Validate-Input () {
     if (Test-Path -Path $WECSubscriptionsPath -PathType Container) {
-        $WECSubscriptionsPath = Resolve-Path $WECSubscriptionsPath
+        $script:WECSubscriptionsPath = Resolve-Path $WECSubscriptionsPath
     } else {
         Write-Error "The provided WEC subscriptions path does not exist: $WECSubscriptionsPath"
+    }
+
+    $script:Subscriptions = Get-ChildItem -Path $WECSubscriptionsPath -Recurse -File -Include "*.xml"
+    if (!($Subscriptions)) {
+        Write-Error "No WEC subscriptions found in the given path: $WECSubscriptionsPath"
+    }
+
+    if ($TimeFilter) {
+        if ($TimeFilter.TotalSeconds -lt 60) {
+            Write-Error "The provided time filter must be at least 60 seconds."
+        }
+        $script:CustomViewsPath = Join-Path $CustomViewsPath (Get-TimeSpanDescription $TimeFilter)
     }
 
     if (Test-Path -Path $CustomViewsPath -PathType Container -IsValid) {
@@ -53,14 +73,19 @@ Function Validate-Input () {
             Write-Verbose "Creating the specified directory to store Custom Views: $CustomViewsPath"
             $null = New-Item -Path $CustomViewsPath -ItemType Directory
         }
-        $CustomViewsPath = Resolve-Path $CustomViewsPath
+        $script:CustomViewsPath = Resolve-Path $CustomViewsPath
     } else {
         Write-Error "The provided Custom Views path is invalid: $CustomViewsPath"
     }
+}
 
-    $script:Subscriptions = Get-ChildItem -Path $WECSubscriptionsPath -Recurse -File -Include "*.xml"
-    if (!($Subscriptions)) {
-        Write-Error "No WEC subscriptions found in the given path: $WECSubscriptionsPath"
+Function Get-TimeSpanDescription ([TimeSpan] $TimeSpan) {
+    if ($TimeSpan.Days -gt 0 ) {
+        return "Last " + $TimeSpan.Days + " Days"
+    } elseif ($TimeSpan.Hours -gt 0) {
+        return "Last " + $TimeSpan.Hours + " Hours"
+    } else {
+        return "Last " + $TimeSpan.Minutes + " Minutes"
     }
 }
 
@@ -97,12 +122,19 @@ Function New-CustomView ([Xml.XmlElement] $SelectElement) {
         return
     }
 
-    # Attempt to extract the Custom View query
-    $CvQuery = Extract-SelectQuery $SelectElement
-    if (!($CvQuery)) {
-        Write-Warning ("Couldn't find the XPath query for Select element:`n" + $SelectElement.OuterXML)
-        return
+    # Extract the XPath query from the element
+    $CvXpath = $SelectElement.InnerText
+
+    # If we've provided a time filter we need to add it
+    if ($TimeFilter) {
+        $TimeDiff = $TimeFilter.TotalMilliseconds
+        $CvXpath += "  and`n      *[System[TimeCreated[timediff(@SystemTime) &lt;= $TimeDiff]]]"
     }
+
+    # The extracted query is for selecting events on remote systems, but
+    # we'll be creating the Custom View on the Event Collector. As such,
+    # we must adjust the provided query to use the Forwarded Events log.
+    $CvQuery = $CvFwdEvtStart + $CvXpath + $CvFwdEvtEnd
 
     # Construct the Custom View
     $CvData = $CvFileStart
@@ -131,18 +163,6 @@ Function Extract-SelectComment ([Xml.XmlElement] $SelectElement) {
     $SelectComment = $SelectElement.PreviousSibling.Innertext.Trim()
     Write-Debug "Found comment of Select element: $SelectComment"
     return $SelectComment
-}
-
-Function Extract-SelectQuery ([Xml.XmlElement] $SelectElement) {
-    $SelectQuery = $SelectElement.InnerText
-    Write-Debug "Found XPath of Select element: $SelectQuery"
-
-    # The extracted query is for selecting events on remote systems, but
-    # we'll be creating the Custom View on the Event Collector. As such,
-    # we must adjust the provided query to use the Forwarded Events log.
-    $CustomQuery = "    <Select Path=`"ForwardedEvents`">" + $SelectQuery + "</Select>`n"
-
-    return $CustomQuery
 }
 
 # Additional sanity checking
